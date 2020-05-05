e58008a76a054fc2f0d3ef5d1f1da1196dbde5bd
jan <janmasrovira@gmail.com>
2020-05-05 22:42:50 +0200

    .

--- get-pdf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl --location "https://gitlab.com/janmasrovira/masterlogic-thesis/-/jobs/artifacts/master/download?job=org2tex" --output artifacts.zip
-unzip artifacts.zip
-latexmk -pdf report
